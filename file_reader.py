2729f43149aab0506f2c427c7bbe7a4e8d090d6d
ayyoubAbrk <abarkachayyoub8@gmail.com>
2026-06-15 12:32:52 +0200

    Update file_reader.py

--- file_reader.py
+++ file_reader.py
@@ -1,12 +1,12 @@
 from pypdf import PdfReader
 import os
 
-reader = PdfReader("cr.pdf")
-text = ""
+reader = PdfReader("cr.pdf") # Initialisation du lecteur PDF avec le fichier "cr.pdf"
+text = "" # Initialisation d'une variable pour stocker le texte extrait
 
-for page in reader.pages:
-    text_page = page.extract_text()
-    if text_page:
+for page in reader.pages: # Parcours de chaque page du fichier .pdf
+    text_page = page.extract_text() # On extrait le texte de la page
+    if text_page: # Si le texte de la page n'est pas vide, on l'ajoute à la variable text
         text += text_page
     
 
