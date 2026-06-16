32558939187d9cea05f1682fde801f46bec70131
ayyoubAbrk <abarkachayyoub8@gmail.com>
2026-06-16 17:27:30 +0200

    Avancée du LLM

--- llm.py
+++ llm.py
@@ -1,6 +1,8 @@
 import os
 from transformers import pipeline
 from pypdf import PdfReader
+import warnings
+warnings.filterwarnings("ignore", message=".*unauthenticated requests.*")
 
 ##__________________ INITIALISATION DES VARIABLES __________________##
 reader = PdfReader("cr.pdf")  # Initialisation du lecteur PDF avec le fichier "cr.pdf"
@@ -15,15 +17,30 @@ for page in reader.pages:  # Parcours de chaque page du fichier .pdf
     if text_page:  # Si le texte de la page n'est pas vide, on l'ajoute à la variable text
         text += text_page
 
-prompt = "Résume le compte rendu suivant en un seul paragraphe concis et clair : " + text  # Création du prompt pour le modèle en ajoutant le texte extrait du PDF
+messages = [
+    {
+        "role": "system", 
+        "content": (
+            "Tu es un médecin vulgarisateur. Résume le document fourni en 3 phrases courtes "
+            "et simples maximum. Sois direct, ne fais pas d'introduction (ne dis pas 'Voici le résumé') "
+            "et va droit au but. Arrête-toi immédiatement après le point final de la 3ème phrase."
+        )
+    },
+    {
+        "role": "user", 
+        "content": f"Voici le document : {text}"
+    }
+]
+
+prompt_final = generator.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
 print("L'IA réfléchit...")  # Message indiquant que le modèle est en train de générer une réponse
 result = generator(
-    prompt, 
+    prompt_final, 
     max_new_tokens=300,   # Amplement suffisant pour un résumé médical
-    max_lenght = None,  # Pas de limite stricte sur la longueur du texte généré
+    max_length = None,  # Pas de limite stricte sur la longueur du texte généré
     do_sample=True, 
-    temperature=0.1,      # Très basse pour forcer l'IA à rester factuelle et sérieuse
+    temperature=0.3,      # Très basse pour forcer l'IA à rester factuelle et sérieuse
     clean_up_tokenization_spaces=True
 )  # Génération de la réponse du modèle avec les paramètres spécifiés
 
